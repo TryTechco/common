069d69811bc7af1c00cae470ccbf6c860be1f2fe
Randy Liu <wert6410@gmail.com>
2021-07-23 14:14:26 +0800

    auth0

--- models/user.interface.ts
+++ models/user.interface.ts
@@ -1,35 +1,35 @@
-import { Social } from "./social.interface";
-import { Order } from "./order.interface";
-import { Asset } from "./asset.interface";
+import { Social } from './social.interface'
+import { Order } from './order.interface'
+import { Asset } from './asset.interface'
 
-export type Role = "admin" | "gallery" | "artist" | "user";
+export type Role = 'admin' | 'gallery' | 'artist' | 'user'
 
 export interface Auth0User {
-  user_id: string;
-  username: string;
-  birthday?: string;
-  email?: string;
-  last_ip?: string;
-  picture?: string;
-  phone_number?: string;
+  sub: string
+  name: string
+  birthday?: string
+  email?: string
+  last_ip?: string
+  picture?: string
+  phone_number?: string
 }
 
 export interface User extends Auth0User {
-  role: Role;
-  social?: Social;
-  wallet_address?: string;
-  orders?: Order[];
-  address?: string;
+  role: Role
+  social?: Social
+  wallet_address?: string
+  orders?: Order[]
+  address?: string
 }
 
 export interface Gallery extends User {
-  role: "gallery";
-  products?: Asset[];
-  artists?: Artist[];
+  role: 'gallery'
+  products?: Asset[]
+  artists?: Artist[]
 }
 
 export interface Artist extends User {
-  role: "artist";
-  collection?: Asset[];
-  boss?: Gallery;
+  role: 'artist'
+  collection?: Asset[]
+  boss?: Gallery
 }
